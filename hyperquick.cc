bf8c2edbd56dd7fbaee403b7abb41666adb8aca7
Axel Fahy <axel@fahy.net>
2015-11-13 11:31:40 +0100

    Version with &

--- hyperquick.cc
+++ hyperquick.cc
@@ -7,12 +7,14 @@
 
 using namespace std;
 
+int n_bit_position(int number,int position);
+
 void reunion(int* data1, int taille1,
              int* data2, int taille2, int* result) {
     int k = 0;
 
     cout << "[reunion] " << endl;
-    // A verifier
+
     for (int i = 0; i < taille1; i++)
         result[k++] = data1[i];
 
@@ -22,60 +24,72 @@ void reunion(int* data1, int taille1,
     sort(result, result + k);
 }
 
-void partitionH(int pivot, int* data, int &taille,
-               int* dataInf,int *taille1,
-               int* dataSup,int *taille2) {
+void partitionH(int& pivot, int* data, int taille,
+               int* dataInf, int& taille1,
+               int* dataSup, int& taille2) {
     int j = 0;
     int k = 0;
+    //int* dataInfTmp = new int[taille * 2];
+    //int* dataSupTmp = new int[taille * 2];
+    //int dataInfTmp[taille * 2];
+    //int dataSupTmp[taille * 2];
 
     for (int i = 0; i < taille; i++) {
-        //cout << "********************* data[i] = " << data[i] << "; pivot = " << pivot << endl;
-        if (data[i] < pivot)
+        if (data[i] <= pivot) {
+            //dataInfTmp[k++] = data[i];
+            dataInf = (int*) realloc (dataInf, (k + 1) * sizeof(int));
             dataInf[k++] = data[i];
-        else
+        }
+        else {
+            //dataSupTmp[j++] = data[i];
+            dataSup = (int*) realloc (dataSup, (j + 1) * sizeof(int));
             dataSup[j++] = data[i];
+        }
     }
-    
-    // cout << "[partition] taille1 = " << k << "; taille2 = " << j << endl;
 
-    *taille1 = k;
-    *taille2 = j;
+    cout << "[partitionH] " << "Pivot : " << pivot << " / k : " << k << " / j : " << j << endl;
+
+    taille1 = k;
+    taille2 = j;
+
+    //dataInf = new int[taille1];
+    //dataSup = new int[taille2];
+    //dataInf = dataInfTmp;
+    //dataSup = dataSupTmp;
 }
 
-void exchange(int* data, int* taille, int etape) {
+void exchange(int* data, int& taille, int etape) {
     int myPE;
     MPI_Comm_rank(MPI_COMM_WORLD,&myPE);
 
     // Find the neighbor
-    int neighbor = myPE ^ (0x1 << (etape - 1));
-    //int neighbor = myPE ^ (int)pow(2, etape);
+    int neighbor = myPE ^ (0x1 << (etape-1));
 
-    cout << "[exchange] " << "myPE : " << myPE << " / size : " << *taille << " / neighbor : " << neighbor << endl;
+    cout << "[exchange] " << "myPE : " << myPE << " / size : " << taille << " / neighbor : " << neighbor << endl;
 
     // Send the size to the neighbor
     //cout << "exchange send size : Send by " << neighbor << " : " << myPE << " et etape : " << etape << " size : " << taille << endl;
-    MPI_Send(taille, 1, MPI_INT, neighbor, 666, MPI_COMM_WORLD);
-    if (*taille > 0) {
+    MPI_Send(&taille, 1, MPI_INT, neighbor, 666, MPI_COMM_WORLD);
+    if (taille > 0) {
         // Send the array to the neighbor
         //cout << "exchange send array : Send by " << neighbor << " : " << myPE << " et etape : " << etape << endl;
-        MPI_Send(data, *taille, MPI_INT, neighbor, 667, MPI_COMM_WORLD);
+        MPI_Send(data, taille, MPI_INT, neighbor, 666, MPI_COMM_WORLD);
     }
 
     // Receive size from the neighbor
     //cout << "Neighbo : Recv by " << neighbor << endl;
-    MPI_Recv(taille, 1, MPI_INT, neighbor, 666, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    if (*taille > 0) {
+    MPI_Recv(&taille, 1, MPI_INT, neighbor, 666, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    if (taille > 0) {
         // Receive array from the neighbor
-        MPI_Recv(data, *taille, MPI_INT, neighbor, 667, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(data, taille, MPI_INT, neighbor, 666, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
-
 }
 
-void diffusion(int *pivot, int etape) {
+void diffusion(int pivot, int etape) {
     // Send pivot depending on the etape
     int p, myPE;
-    MPI_Comm_size(MPI_COMM_WORLD,&p);
-    MPI_Comm_rank(MPI_COMM_WORLD,&myPE);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myPE);
 
     cout << "[diffusion] : " << "myPE : " << myPE << " / etape : " << etape << endl;
 
@@ -85,22 +99,18 @@ void diffusion(int *pivot, int etape) {
         root = 0;
     else
         root = ((myPE >> etape) << etape);
-    
-    // cout << "[diffusion] The root is : " << root << endl;
-    
+
     // myPE - root = relative index in sub-hypercude of dimension i
     for (int k = 0; k < etape; k++) {
         if ((myPE - root) < (0x1 << k)) {
             //cout << "Send by " << myPE << " : " << myPE + (0x1 << k) << " et etape : " << etape << endl;
             // Send the pivot
-            cout << "[diffusion] : I send the pivot : " << *pivot << " to my friend : " << myPE + (0x1 << k) <<  endl;
-            MPI_Send(pivot, 1, MPI_INT, myPE + (0x1 << k), 668, MPI_COMM_WORLD);
+            MPI_Send(&pivot, 1, MPI_INT, myPE + (0x1 << k), 666, MPI_COMM_WORLD);
         }
         else if ((myPE - root) < (0x1 << (k + 1))) {
             // Receive the pivot
             //cout << "Diffusion : Recv by " << myPE - (0x1 << k) <<  " et etape : " << etape << endl;
-            MPI_Recv(pivot, 1, MPI_INT, myPE - (0x1 << k), 668, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            cout << "[diffusion] : I " << myPE << " received a new pivot : " << *pivot << " from my friend : " << myPE - (0x1 << k) << endl;
+            MPI_Recv(&pivot, 1, MPI_INT, myPE - (0x1 << k), 666, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
     }
 }
@@ -114,11 +124,13 @@ void quickSort(int* data, int& taille) {
 
     // Dimension
     int d = log2(p);
-    // cout << "Dimension(d) : " << d << endl;
+    cout << "Dimension(d) : " << d << endl;
 
     // Tableau de resultat
     int* dataInf = new int[taille];
     int* dataSup = new int[taille];
+    //int* dataInf = NULL;
+    //int* dataSup = NULL;
     int tailleInf = 0;
     int tailleSup = 0;
     int pivot = 0;
@@ -129,24 +141,29 @@ void quickSort(int* data, int& taille) {
             pivot = data[taille / 2];
             cout << "Pivot[" << i << "] = " << pivot << endl;
         }
-        dataInf = new int[taille];
-        dataSup = new int[taille];
 
         // Call diffusion
-        diffusion(&pivot, i);
-
-        // cout << "         " << myPE << "        New pivot = " << pivot << endl;
+        diffusion(pivot, i);
 
         // Partition
-        partitionH(pivot, data, taille, dataInf, &tailleInf, dataSup, &tailleSup);
-
-        cout << "I am " << myPE << " and tailleInf = " << tailleInf << "; and tailleSup = " << tailleSup << endl;
+        partitionH(pivot, data, taille, dataInf, tailleInf, dataSup, tailleSup);
+        cout << "[quicksort] : " << myPE << " : tailleInf : " << tailleInf << " / tailleSup : " << tailleSup << endl;
+        //cout << "[quicksort] : " << "tailleInf : " << tailleInf << " / tailleSup : " << tailleSup << endl;
+        //cout << "[quicksort] : " << "data    ---" << endl;
+        //for (int k = 0; k < taille; k++) cout << data[k] << endl;
 
         // Exchange of arrays
-        if (!(myPE & (0x1 << (i - 1))))
-            exchange(dataSup, &tailleSup, i);
+        //if (!(myPE & (0x1 << (i - 1))))
+        //if (n_bit_position(myPE, i-1))
+        if (!(myPE >> (i - 1)) & 0x1)
+            exchange(dataSup, tailleSup, i);
         else
-            exchange(dataInf, &tailleInf, i);
+            exchange(dataInf, tailleInf, i);
+
+        //cout << "[quicksort] : " << "dataInf ---" << endl;
+        //for (int k = 0; k < tailleInf; k++) cout << dataInf[k] << endl;
+        //cout << "[quicksort] : " << "dataSup ---" << endl;
+        //for (int k = 0; k < tailleSup; k++) cout << dataSup[k] << endl;
 
         // Reunion de listes
         //int result = new int[(int *)(taille) + tailleNeighbor];
@@ -156,7 +173,47 @@ void quickSort(int* data, int& taille) {
     }
 }
 
-void printAll(int* data, int taille) {
+int n_bit_position(int number,int position)
+{
+    return (number>>(position));
+}
+
+void printAll(int* data,int taille) {
+   int p,myPE;
+   MPI_Comm_size(MPI_COMM_WORLD,&p);
+   MPI_Comm_rank(MPI_COMM_WORLD,&myPE);
+   int *recvcounts, *displs, *recvbuf;
+   if (myPE == 0) recvcounts = new int[p];
+   MPI_Gather(&taille,1,MPI_INT,recvcounts,1,MPI_INT,0,MPI_COMM_WORLD);
+   if (myPE == 0) {
+      displs = new int[p];
+      displs[0] = 0;
+      for (int pe=1;pe<p;pe++) displs[pe] = displs[pe-1]+recvcounts[pe-1];
+   }
+   if (myPE == 0) recvbuf = new int[displs[p-1]+recvcounts[p-1]];
+   MPI_Gatherv(data,taille,MPI_INT,recvbuf,recvcounts,displs,MPI_INT,
+               0,MPI_COMM_WORLD);
+   if (myPE == 0)
+      for (int k=0;k<displs[p-1]+recvcounts[p-1];k++) cout << recvbuf[k] << endl;
+   if (myPE == 0) delete recvbuf,recvcounts,displs;
+}
+
+int main(int argc,char** argv) {
+  MPI_Init(&argc,&argv);
+  int myPE;
+  MPI_Comm_rank(MPI_COMM_WORLD,&myPE);
+  int seed = atoi(argv[1])+myPE;
+  srand(seed);
+  int tailleLoc = atoi(argv[2]); // tailleLoc vaut n/p
+  int* dataLoc = new int[tailleLoc];
+  for (int k=0;k<tailleLoc;k++) dataLoc[k] = rand()%1000;
+  quickSort(dataLoc,tailleLoc);
+  printAll(dataLoc,tailleLoc);
+  MPI_Finalize();
+  return 0;
+}
+
+/* void printAll(int* data, int taille) {
    int p, myPE;
    MPI_Comm_size(MPI_COMM_WORLD, &p);
    MPI_Comm_rank(MPI_COMM_WORLD, &myPE);
@@ -196,4 +253,4 @@ int main(int argc, char** argv) {
 
   MPI_Finalize();
   return 0;
-}
+}*/
